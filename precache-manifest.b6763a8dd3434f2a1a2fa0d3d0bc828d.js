a8b57965307ec5c85c19a8d1470ca6910572ca55
jezzicaaaaa <jezzicamae@gmail.com>
2020-04-08 12:38:41 -0400

    Updates

---
+++ precache-manifest.b6763a8dd3434f2a1a2fa0d3d0bc828d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bf0033d984dbac3a00707305e5ac2a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c39479bca2ec4c3fda5a",
+    "url": "/monster-rolodex/static/css/main.becffa23.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/monster-rolodex/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/monster-rolodex/static/js/2.91e906d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c39479bca2ec4c3fda5a",
+    "url": "/monster-rolodex/static/js/main.a0692320.chunk.js"
+  },
+  {
+    "revision": "238059c1b43ed123953f",
+    "url": "/monster-rolodex/static/js/runtime-main.14143c00.js"
+  }
+]);
